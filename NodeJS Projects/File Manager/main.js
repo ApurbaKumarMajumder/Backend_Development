bc14029b5194dec612838c0ed71b395d7cbeec23
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-31 23:58:40 +0530

    working code for the file organizer

--- NodeJS Projects/File Manager/main.js
+++ NodeJS Projects/File Manager/main.js
@@ -4,6 +4,13 @@ const path = require("path");
 let inputArr = process.argv.slice(2);
 console.log(inputArr);
 
+let types = {
+    media: ["mp4", "mkv", "png", "jpeg"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+
 // node main.js tree "directoryPath"
 // node main.js organize "directoryPath"
 // node main.js help
@@ -54,25 +61,59 @@ function organizeFn(dirPath) {
 	}
 
 	organizeHelper(dirPath, destPath);
-
-	// 4. copy / cut files to that organized directory inside of any of category folder.
 }
 
-function organizeHelper(dirPath, destPath) {
+function organizeHelper(src, dest) {
     
     // 3. identify categories of all the files preesent in that input directory
 
     let childNames = fs.readdirSync(src);
-    console.log(childNames);
+    // console.log(childNames);
 
     for (let i = 0; i < childNames.length; i++) {
         let childAddress = path.join(src, childNames[i]);
         let isFile = fs.lstatSync(childAddress).isFile();
         if (isFile) {
-            console.log(childNames[i]);
+            // console.log(childNames[i]);
+            let category = getCategory(childNames[i]);
+            // console.log(childNames[i], "belongs to --> ", category);
+            sendFiles(childAddress, dest, category);
         }
     }
 };
+
+function sendFiles(srcFilePath, dest, category) {
+    
+    // first create all the categories in the organized_files folder
+    let categoryPath = path.join(dest, category);
+
+    if (fs.existsSync(categoryPath) == false) {
+        fs.mkdirSync(categoryPath);
+    }
+
+    let fileName = path.basename(srcFilePath);
+    let destFilePath = path.join(categoryPath, fileName);
+    fs.copyFileSync(srcFilePath, destFilePath);
+    fs.unlinkSync(srcFilePath);
+    console.log(fileName, "copied to ", category);
+}
+
+function getCategory(name) {
+    let ext = path.extname(name);
+    ext = ext.slice(1);
+
+    for (let type in types) {
+        let currentArrayType = types[type];
+
+        for (let i = 0; i < currentArrayType.length; i++) {
+            if (ext == currentArrayType[i]) {
+                return type; // return archive, app and others
+            }
+        }
+    }
+
+    return "others";
+}
 
 // help implemented
 function helpFn() {
