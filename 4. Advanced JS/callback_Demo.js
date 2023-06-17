be9046a3f423baf99ab267ab7627c0dc1763f9ea
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-06-17 23:38:02 +0530

    starting with callbacks

---
+++ 4. Advanced JS/callback_Demo.js
@@ -0,0 +1,17 @@
+function fun(x, fn) {
+    /**
+     * x -> number
+     * fn -> callback function 
+     */
+    // some logic
+    for (let i = 0; i < x; i++) {
+        console.log(i);        
+    }
+    fn(); // called the callback function passed
+    // some more logic
+}
+
+fun(10, function log() {
+    // this is the callback function
+    console.log("Custom logger");
+});
